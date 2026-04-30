cc14964746cc82d0162d7b238766f1f1c0db7dba
Mohammed Yasser <memomohammed.2005@gmail.com>
2026-04-30 15:09:27 +0300

    feat: add cell api

---
+++ backend/routers/cell.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, status, HTTPException
+from sqlmodel import text
+import schemas
+from database import SessionDep
+
+router = APIRouter(
+    prefix="/cell",
+    tags=["cell"]
+)
+
+@router.get("/{cell_id}", status_code=status.HTTP_200_OK, response_model=schemas.CellResponse)
+def get_cell_by_id(cell_id: int, db: SessionDep):
+    cell = db.execute(text("""
+        SELECT 
+            c.*,
+            c.capacity AS total_capacity,
+            (
+                SELECT COUNT(*)
+                FROM inmate i
+                WHERE i.assigned_cell = c.cell_id
+            ) AS current_occupancy
+        FROM cell c
+        WHERE c.cell_id = :id
+    """), {"id": cell_id}).fetchone()
+    if not cell:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Cell with id {cell_id} not found")
+    return cell
+
+@router.post("", status_code=status.HTTP_201_CREATED, response_model=schemas.CellResponse)
+def create_cell(request: schemas.CellCreate, db: SessionDep):
+    # Check if the block exists
+    block = db.execute(text("SELECT * FROM block WHERE block_id = :id"), {"id": request.block_id}).fetchone()
+    if not block:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Block with id {request.block_id} not found")
+
+    result = db.execute(text("""
+        INSERT INTO cell (block_id, capacity)
+        VALUES (:block_id, :capacity)
+        RETURNING *
+    """), request.model_dump())
+    
+    new_cell = result.fetchone()
+    db.commit()
+
+    # Get the created cell with the calculated fields
+    return get_cell_by_id(new_cell.cell_id, db)
