f5f380b8668ee9d6319b5aa2c16f681bc69892fb
Mohammed Yasser <memomohammed.2005@gmail.com>
2026-05-01 01:30:29 +0300

    feat: add staff route

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from database import create_db_and_tables
 from routers import prison
 from routers import block
 from routers import cell
+from routers import staff
 import models
 
 app = FastAPI()
@@ -16,5 +17,4 @@ def on_startup():
 app.include_router(prison.router)
 app.include_router(block.router)
 app.include_router(cell.router)
-
-
+app.include_router(staff.router)
