551ee404fdce7dc3bab6a026aea69aa166f114dc
Mohammed Yasser <memomohammed.2005@gmail.com>
2026-04-30 15:22:30 +0300

    fix: fix main

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI
 from database import create_db_and_tables
-from routers import prison, routers
+from routers import prison
+from routers import block
+from routers import cell
 import models
 
 app = FastAPI()
 
-# Add all routers
-for router in routers:
-    app.include_router(router)
+
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
 app.include_router(prison.router)
+app.include_router(block.router)
+app.include_router(cell.router)
 
 
