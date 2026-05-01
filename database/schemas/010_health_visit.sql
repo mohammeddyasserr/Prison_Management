3dc70d79c74c42f98ba959ef0e16a2e105b9c1ef
Mohammed Yasser <memomohammed.2005@gmail.com>
2026-05-01 16:22:54 +0300

    fix: fix id and phone in schema

--- database/schemas/010_health_visit.sql
+++ database/schemas/010_health_visit.sql
@@ -1,7 +1,7 @@
 CREATE TABLE health_visit (
     visit_id        INTEGER         PRIMARY KEY AUTOINCREMENT,
     inmate_id       INTEGER         NOT NULL REFERENCES inmate(inmate_id) ON DELETE CASCADE,
-    doctor_id       VARCHAR(14)         NOT NULL REFERENCES doctor(national_id) ON DELETE RESTRICT,
+    doctor_id       CHAR(14)         NOT NULL REFERENCES doctor(national_id) ON DELETE RESTRICT,
     visit_datetime  TIMESTAMP       NOT NULL,
     diagnosis       TEXT            NOT NULL
 );
--- database/schemas/016_shift.sql
+++ database/schemas/016_shift.sql
@@ -2,6 +2,6 @@ CREATE TABLE Shift (
     shift_id        INTEGER         PRIMARY KEY AUTOINCREMENT,
     shift_type      VARCHAR(20)     NOT NULL
                     CHECK (shift_type IN ('Morning','Afternoon','Night')),
-    officer_id      VARCHAR(14)         NOT NULL REFERENCES officer(national_id) ON DELETE RESTRICT,
-    manager_id      VARCHAR(14)         NOT NULL REFERENCES officer(national_id) ON DELETE RESTRICT
+    officer_id      CHAR(14)         NOT NULL REFERENCES officer(national_id) ON DELETE RESTRICT,
+    manager_id      CHAR(14)         NOT NULL REFERENCES officer(national_id) ON DELETE RESTRICT
 );
