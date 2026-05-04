76714508c8cdeec1b7553a26c52d6ae7b2ebd167
malaakk1 <malaakk81@gmail.com>
2026-05-05 02:33:46 +0300

    feat:inc_disc_edits

--- backend/schemas/disciplinary.py
+++ backend/schemas/disciplinary.py
@@ -12,33 +12,33 @@ class PunishmentType(str, Enum):
 
 
 class DisciplinaryCreate(SQLModel):
-    inmate_id: int
-    incident_id: Optional[int] = None
-    imposed_by: str
+    inmate_id:       int
+    incident_id:     int | None = None
+    imposed_by:      str                 
     punishment_type: PunishmentType
-    solitary_days: Optional[int] = None
-    date_imposed: date
-    notes: Optional[str] = None
+    solitary_days:   int | None = None   
+    date_imposed:    date
+    notes:           str | None = None
 
 
 class DisciplinaryUpdate(SQLModel):
-    inmate_id: Optional[int] = None
-    incident_id: Optional[int] = None
-    imposed_by: Optional[str] = None
-    punishment_type: Optional[PunishmentType] = None
-    solitary_days: Optional[int] = None
-    date_imposed: Optional[date] = None
-    notes: Optional[str] = None
+    inmate_id:       int                 
+    imposed_by:      str                 
+    incident_id:     int | None = None   
+    punishment_type: PunishmentType | None = None
+    solitary_days:   int | None = None
+    date_imposed:    date | None = None
+    notes:           str | None = None
 
 
 class DisciplinaryResponse(SQLModel):
-    log_id: int
-    inmate_id: int
-    inmate_name: str
-    incident_id: Optional[int]
-    imposed_by: str
-    officer_name: str
+    inmate_id:       int
+    inmate_name:     str | None = None   
+    incident_id:     int | None = None
+    imposed_by:      str
+    officer_name:    str | None = None   
     punishment_type: str
-    solitary_days: Optional[int]
-    date_imposed: date
-    notes: Optional[str]
+    solitary_days:   int | None = None
+    date_imposed:    date
+    end_date:        date | None = None  
+    notes:           str | None = None
