12720448b15ca85e6d51f995a9d4cd6189cea017
Mohammed Yasser <memomohammed.2005@gmail.com>
2026-05-05 23:49:33 +0300

    fix : fix timeslot table

--- database/migrations/002_add_date_to_time_slots.sql
+++ database/migrations/002_add_date_to_time_slots.sql
@@ -1,2 +1,2 @@
-ALTER TABLE time_slots ADD COLUMN date DATE;
-ALTER TABLE time_slots ADD CONSTRAINT unique_date_time_slot UNIQUE (date, start_time, end_time);
+ALTER TABLE timeslot ADD COLUMN date DATE;
+ALTER TABLE timeslot ADD CONSTRAINT unique_date_timeslot UNIQUE (date, start_time, end_time);
