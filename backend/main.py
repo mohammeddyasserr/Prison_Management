701c1bb4e8eaae03d14f6494886fa74f0bcbb494
Mohammed Yasser <memomohammed.2005@gmail.com>
2026-05-05 13:42:15 +0300

    feat : add CORS

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import create_db_and_tables
 from routers import prison
 from routers import block
@@ -18,7 +19,13 @@ import models
 
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
