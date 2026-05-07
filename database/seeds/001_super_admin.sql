e0115a0ef94bf2cf87f15d84ff845dcf21e8e239
pavly111 <pavly.nader1@gmail.com>
2026-05-07 15:45:35 +0300

    names of admin

--- database/seeds/001_super_admin.sql
+++ database/seeds/001_super_admin.sql
@@ -1,7 +1,7 @@
 INSERT INTO super_admin (national_id, name, phone, address, email, password_hash)
 VALUES 
-('10101010101010', 'Admin First', '01011111111', '1 Main St, Cairo', 'admin1@sys.com', '$argon2id$v=19$m=65536,t=3,p=4$66iZU0XU5nx+kTHKUCEbYw$GYsXuv8a1pz535U+mzNb/zWsKNrCLUaPS9MEPv/pgWU'),
-('20202020202020', 'Admin Second', '01222222222', '2 Admin Rd, Giza', 'admin2@sys.com', '$argon2id$v=19$m=65536,t=3,p=4$66iZU0XU5nx+kTHKUCEbYw$GYsXuv8a1pz535U+mzNb/zWsKNrCLUaPS9MEPv/pgWU'),
-('30303030303030', 'Admin Third', '01533333333', '3 Tower St, Alex', 'admin3@sys.com', '$argon2id$v=19$m=65536,t=3,p=4$66iZU0XU5nx+kTHKUCEbYw$GYsXuv8a1pz535U+mzNb/zWsKNrCLUaPS9MEPv/pgWU'),
-('40404040404040', 'Admin Fourth', '01144444444', '4 Nile St, Mansoura', 'admin4@sys.com', '$argon2id$v=19$m=65536,t=3,p=4$66iZU0XU5nx+kTHKUCEbYw$GYsXuv8a1pz535U+mzNb/zWsKNrCLUaPS9MEPv/pgWU'),
-('50505050505050', 'Admin Fifth', '01055555555', '5 Sea St, Suez', 'admin5@sys.com', '$argon2id$v=19$m=65536,t=3,p=4$66iZU0XU5nx+kTHKUCEbYw$GYsXuv8a1pz535U+mzNb/zWsKNrCLUaPS9MEPv/pgWU');
+('10101010101010', 'Antony Wassem', '01011111111', '1 Main St, Cairo', 'admin1@sys.com', '$argon2id$v=19$m=65536,t=3,p=4$66iZU0XU5nx+kTHKUCEbYw$GYsXuv8a1pz535U+mzNb/zWsKNrCLUaPS9MEPv/pgWU'),
+('20202020202020', 'Ahmed Mohsen', '01222222222', '2 Admin Rd, Giza', 'admin2@sys.com', '$argon2id$v=19$m=65536,t=3,p=4$66iZU0XU5nx+kTHKUCEbYw$GYsXuv8a1pz535U+mzNb/zWsKNrCLUaPS9MEPv/pgWU'),
+('30303030303030', 'Mohammed yasser', '01533333333', '3 Tower St, Alex', 'admin3@sys.com', '$argon2id$v=19$m=65536,t=3,p=4$66iZU0XU5nx+kTHKUCEbYw$GYsXuv8a1pz535U+mzNb/zWsKNrCLUaPS9MEPv/pgWU'),
+('40404040404040', 'Amina Ahmed', '01144444444', '4 Nile St, Mansoura', 'admin4@sys.com', '$argon2id$v=19$m=65536,t=3,p=4$66iZU0XU5nx+kTHKUCEbYw$GYsXuv8a1pz535U+mzNb/zWsKNrCLUaPS9MEPv/pgWU'),
+('50505050505050', 'Omar Gamal', '01055555555', '5 Sea St, Suez', 'admin5@sys.com', '$argon2id$v=19$m=65536,t=3,p=4$66iZU0XU5nx+kTHKUCEbYw$GYsXuv8a1pz535U+mzNb/zWsKNrCLUaPS9MEPv/pgWU');
